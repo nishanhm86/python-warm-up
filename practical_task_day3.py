fa254093d38f703edd859c09980fcc4705e00f1b
Nishan Maduranga <harsha.leisureaqua@gmail.com>
2025-10-14 12:17:40 +0530

    New changes to the code

--- practical_task_day3.py
+++ practical_task_day3.py
@@ -8,10 +8,11 @@ while True:
     print("2. Display Numbers")
     print("3. Display Total")
     print("4. Display Average")
-    print("5.Find Maximum and Minimum")
-    print("6.Exit")
+    print("5. Find Maximum and Minimum")
+    print("6. Remove a Number")
+    print("7. Exit")
 
-    choice = input("Enter your choice(1-6): ")
+    choice = input("Enter your choice(1-7): ")
     if choice == "1":
         n = int(input("Enter how many numbers to add: "))
         for i in range(n):
@@ -46,6 +47,22 @@ while True:
             print("Minimum:", min(numbers))
 
     elif choice == "6":
+        if len(numbers) == 0:
+            print("No numbers added yet!")
+        else:
+            print("Numbers Entered:", numbers)
+            try:
+                index = input("Enter index to remove number: ")
+                if 0 <= int(index) < len(numbers):
+                    removed_number = numbers.pop(int(index))
+                    print(f"Numbers {removed_number} Removed Successfully!")
+                    print("Updated list", numbers)
+                else:
+                    print("Invalid index provided!")
+            except ValueError:
+                print("Please enter a valid index!")
+
+    elif choice == "7":
             print("Goodbye. Leaving the program.")
             break
 
